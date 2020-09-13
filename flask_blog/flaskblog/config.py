b6bf3ba7440fdc845784db39a76c27ae1711c124
brighton hoods <47972490+BrightonOtieno@users.noreply.github.com>
2020-09-13 11:30:54 +0300

    Update config.py

--- flask_blog/flaskblog/config.py
+++ flask_blog/flaskblog/config.py
@@ -1,5 +1,5 @@
 class Config:
-    SECRET_KEY='c974b7d854384bf6fcfc2ec1b648f948'
+   
     SQLALCHEMY_DATABASE_URI='sqlite:///site.db'
     #app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
     #app.config['MAIL_PORT'] = 587
